427229b8f6b4a5bc9f4a2c061614348f6628f9c6
abhilash2525 <abhilash.mahajan2525@gmail.com>
2022-07-23 16:51:20 +0530

    modified ser.js file

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ var app = express();
 
 // define routes here...
 app.use(express.static(path.join(__dirname, '/dist/DemoHeroku')))
-app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname+'/dist/DemoHeroku/index.html'))
+app.get('*', function(req, res){
+    //res.sendFile(path.join(__dirname+'/dist/DemoHeroku/index.html'))
+    res.sendFile(path.join(__dirname+'./dist/index.html'))
 })
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000,
     () => console.log("Server is running..."));
+
+
+    
