52b865fa0ad9e62024f941842ecf300f644700cf
abhilash2525 <abhilash.mahajan2525@gmail.com>
2022-07-23 16:30:38 +0530

    updates to deploy to heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+// define routes here...
+app.use(express.static(path.join(__dirname, '/dist/DemoHeroku')))
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname+'/dist/DemoHeroku/index.html'))
+})
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000,
+    () => console.log("Server is running..."));
