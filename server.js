677ca8515e6d19015947827e148d37ac6ed5a206
abhilash2525 <abhilash.mahajan2525@gmail.com>
2022-07-23 16:55:49 +0530

    updated server.js file

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-var express = require('express');
-var path = require('path');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-var app = express();
+const app = express();
 
-// define routes here...
-app.use(express.static(path.join(__dirname, '/dist/DemoHeroku')))
-app.get('*', function(req, res){
-    //res.sendFile(path.join(__dirname+'/dist/DemoHeroku/index.html'))
-    res.sendFile(path.join(__dirname+'./dist/index.html'))
-})
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/DemoHeroku'));
 
-// start the server listening for requests
-app.listen(process.env.PORT || 3000,
-    () => console.log("Server is running..."));
-
-
+app.get('/*', function(req,res) {
     
+res.sendFile(path.join(__dirname+'/dist/DemoHeroku/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
